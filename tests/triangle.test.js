824d8d975e15d919cfc849e852fb6d455a39f738
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-28 15:59:55 -0500

    added a test attempt

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -11,6 +11,14 @@ const shape = new Triangle();
 // })
 
 test('should throw error if render() is called', () => {
-    shape.setColor("blue");
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+    shape.render("BTA","green","blue");
+    expect(shape.render()).toEqual(`
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+<polygon points="200,50 300,200 100,200" fill="green" />
+
+<text x="200" y="175" font-size="60" text-anchor="middle" fill="blue">BTA</text>
+
+</svg>`);
 })
