5d34c65441e6ece5a408227ffd9c114e53d09087
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-28 18:01:07 -0500

    testing on triangle working properly

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -1,24 +1,19 @@
 const Triangle = require('../lib/shapes/triangle.js')
 
-const shape = new Triangle();
-// describe('Triangle', () => {
-//     describe('triangle', () => {
-//         it('should throw error if render() is called', () => {
-//             shape.setColor("blue");
-//             expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-//         })
-//     })
-// })
 
-test('should throw error if render() is called', () => {
 
-    shape.render("BTA","green","blue");
-    expect(shape.render()).toEqual(`
-    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+describe('Triangle', () => {
+    describe('shape', () => {
+        it('should throw error if render() is called', () => {
+            const shape = new Triangle();
+            
+            expect(shape.render("BTA", "green", "blue")).toBe(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-<polygon points="200,50 300,200 100,200" fill="green" />
+<polygon points="200,50 300,200 100,200" fill="blue" />
 
-<text x="200" y="175" font-size="60" text-anchor="middle" fill="blue">BTA</text>
+<text x="200" y="175" font-size="60" text-anchor="middle" fill="green">BTA</text>
 
 </svg>`);
-})
+        });
+    });
+});
