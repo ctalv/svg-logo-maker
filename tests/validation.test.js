edccbebdfc4a2cd267fff044be85362ec5cbc922
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-29 02:14:00 -0500

    working validatin in cli

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -3,6 +3,5 @@ const Validate = require('../lib/validation.js');
 test('if color is a named color, should be true', () => {
     const validate = new Validate('NA','blue','NA');
     
-    
     expect(validate.colorTextValidation()).toBe(true);
 })
