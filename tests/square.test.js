c74c0b538fd4b9985787be65e907cc3d355ff47a
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-28 23:48:47 -0500

    all testing working

--- tests/square.test.js
+++ tests/square.test.js
@@ -2,13 +2,6 @@ const Square = require('../lib/shapes/square.js')
 
 test('should throw error if render() is called', () => {
     const shape = new Square();
-
-    expect(shape.render("BTA", "green", "blue")).toBe(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-<rect width="200" height="200" fill="blue" />
-    
-<text x="100" y="100" font-size="60" text-anchor="middle" fill="green">BTA</text>
-    
-</svg>`);
-
-});
+    shape.setColor("blue");
+    expect(shape.render()).toEqual('<rect width="200" height="200" fill="blue" />');
+})
