e897cc3706e31fa94ce94c86d097f4f382f21434
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-28 23:38:20 -0500

    fixed code for square and circle

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -2,13 +2,6 @@ const Circle = require('../lib/shapes/circle.js')
 
 test('should throw error if render() is called', () => {
     const shape = new Circle();
-
-    expect(shape.render("BTA", "green", "blue")).toBe(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-<circle cx="150" cy="100" r="80" fill="blue" />
-                  
-<text x="150" y="120" font-size="60" text-anchor="middle" fill="green">BTA</text>
-                  
-</svg>`);
-
-});
+    shape.setColor("blue");
+    expect(shape.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue" />');
+})
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -1,18 +1,5 @@
 const Triangle = require('../lib/shapes/triangle.js')
 
-// test('should throw error if render() is called', () => {
-//             const shape = new Triangle();
-
-//             expect(shape.render("BTA", "green", "blue")).toBe(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-// <polygon points="200,50 300,200 100,200" fill="blue" />
-
-// <text x="200" y="175" font-size="60" text-anchor="middle" fill="green">BTA</text>
-
-// </svg>`);
-
-// });
-
 test('should throw error if render() is called', () => {
     const shape = new Triangle();
     shape.setColor("blue");
