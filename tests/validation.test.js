415344b56f434b2b8702a1811a48e6f763e67acc
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-29 02:40:35 -0500

    removed cached old files

--- tests/validation.test.js
+++
@@ -1,7 +0,0 @@
-const Validate = require('../lib/validation.js');
-
-test('if color is a named color, should be true', () => {
-    const validate = new Validate('NA','blue','NA');
-    
-    expect(validate.colorTextValidation()).toBe(true);
-})
